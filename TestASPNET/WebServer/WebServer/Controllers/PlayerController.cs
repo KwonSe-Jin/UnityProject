146ab83de190bc381668d5efb8bf04c33e90d13c
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2025-02-05 22:57:35 +0900

    프로젝트 이전

---
+++ TestASPNET/WebServer/WebServer/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PlayerController : ControllerBase
+	{
+		[HttpGet("{id}")]
+		public IActionResult GetPlayerInfo(int id)
+		{
+			return Ok(new { id, name = "Player1", level = 10 });
+		}
+	}
+}
---
+++ TestASPNET/WebServer/WebServer/WebSocketHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace WebServer
+{
+	public class WebSocketHandler
+	{
+		private readonly RequestDelegate _next;
+
+		public WebSocketHandler(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (context.WebSockets.IsWebSocketRequest)
+			{
+				using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+				await HandleWebSocketCommunication(webSocket);
+			}
+			else
+			{
+				await _next(context);
+			}
+		}
+
+		private async Task HandleWebSocketCommunication(WebSocket webSocket)
+		{
+			var buffer = new byte[1024 * 4];
+			while (webSocket.State == WebSocketState.Open)
+			{
+				var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+				if (result.MessageType == WebSocketMessageType.Text)
+				{
+					var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+					Console.WriteLine($"Received: {message}");
+				}
+			}
+		}
+	}
+}
